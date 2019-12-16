e125a1175d4844f98dc38ae6f672eb59ec30fc6a
Yiying Zou <yiyingzou@gmail.com>
2019-12-16 12:46:49 -0500

    more styling, nav added in jquery

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,25 @@
+const portfolio = {};
+
+portfolio.init = () => {
+  // EVENT HANDLERS
+
+  // scroll down on header
+  $(".scroll-down").on("click", function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#about").offset().top
+      },
+      500
+    );
+  });
+
+  // toggle navigation menu
+  $(".nav__toggle").on("click keypress", function() {
+    $("nav").toggleClass("nav-visible");
+  });
+};
+
 $(function() {
+  portfolio.init();
   AOS.init();
-  console.log("it's workign!");
 });
