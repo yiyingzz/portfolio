989b2defdef6ea308253157ebb78330f4d688215
Yiying Zou <yiyingzou@gmail.com>
2019-12-17 13:21:08 -0500

    more tweaking, nav slide working with jquery

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const portfolio = {};
 
+portfolio.menuOpen = false;
+
 portfolio.init = () => {
   // EVENT HANDLERS
 
@@ -14,17 +16,39 @@ portfolio.init = () => {
   });
 
   // toggle navigation menu
-  $(".nav__toggle").on("click keypress", function() {
-    $("nav").toggleClass("nav-visible");
+  $(".nav__toggle").on("click", function() {
+    if (portfolio.menuOpen === false) {
+      $("nav").addClass("nav-visible");
+      setTimeout(() => {
+        $("nav").removeClass("closed");
+        $("nav").toggleClass("open");
+      }, 100);
+      portfolio.menuOpen = true;
+      console.log(portfolio.menuOpen);
+    } else {
+      $("nav").addClass("closed");
+      setTimeout(() => {
+        $("nav").removeClass("open nav-visible");
+      }, 300);
+      portfolio.menuOpen = false;
+      console.log(portfolio.menuOpen);
+    }
   });
 
   // close menu when a link is clicked
   $("nav li").on("click keypress", function() {
-    $("nav").toggleClass("nav-visible");
+    $("nav").addClass("closed");
+    setTimeout(() => {
+      $("nav").removeClass("open nav-visible");
+    }, 300);
+    portfolio.menuOpen = false;
+    console.log(portfolio.menuOpen);
   });
 };
 
 $(function() {
   portfolio.init();
-  AOS.init();
+  AOS.init({
+    disable: "phone"
+  });
 });
